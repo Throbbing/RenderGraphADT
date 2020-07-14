923f64035ff651a56e42f16bf2230c3824be208a
Throbbing <1015640199@qq.com>
2020-07-15 01:41:17 +0800

    first commit

---
+++ include/RenderPass.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/*
+********************************************
+	Render Graph ADT 
+	出于简化开发流程的考虑，暂不考虑实际的 图形API，
+	而是从设计角度完成 ADT 的编写
+					-- liangshenge
+********************************************
+*/
+
+
+// Render Pass Interface
+class IRenderGraphPass
+{
+public:
+	virtual bool Setup();
+};
